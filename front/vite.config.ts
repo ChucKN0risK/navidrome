9cd7c5d9d2b83e9a85eb36625d18a801e0dabf95
Louis Chenais <lchenais@gmail.com>
2023-08-30 00:31:49 +0200

    Add postcss-nesting

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [postcssNesting]
     }
   },
   server: {
