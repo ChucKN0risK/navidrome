a8dcc9efdb3f2278beadb9d8a97de77f3ecbcfea
Louis Chenais <lchenais@gmail.com>
2024-08-06 12:13:44 +0200

    Improve timeConverter util function typing

--- front/src/utils/timeConverter.utils.ts
+++ front/src/utils/timeConverter.utils.ts
@@ -1,20 +1,21 @@
-/**
- * @name secondsToHHMMSS
- * @description Convert seconds from a number to formatted string
- * @param {Number} totalSeconds
- * @return {String}
- */
+/** @name secondsToHHMMSS */
+/** @description Convert seconds from a number to formatted string */
+/** @param {Number} totalSeconds */
+/** @return {String} */
 export const secondsToHHMMSS = (totalSeconds: number) => {
-  let hours = Math.floor(totalSeconds / 3600);
-  let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
-  let seconds = totalSeconds - (hours * 3600) - (minutes * 60);
-
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+  const seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+  
+  let formattedHours = `${hours}`;
+  let formattedMin = `${minutes}`;
+  let formattedSec = `${seconds}`;
   // Padding the values to ensure they are two digits
-  if (hours < 10) { hours = '0' + hours; }
-  if (minutes < 10) { minutes = '0' + minutes; }
-  if (seconds < 10) { seconds = '0' + seconds; }
+  if (hours < 10) { formattedHours = `0${hours}`; }
+  if (minutes < 10) { formattedMin = `0${minutes}`; }
+  if (seconds < 10) { formattedSec = `0${seconds}`; }
 
   return hours < 1 ?
-    `${minutes}:${seconds}` :
-    `${hours}:${minutes}:${seconds}`;
+    `${formattedMin}:${formattedSec}` :
+    `${formattedHours}:${formattedMin}:${formattedSec}`;
 }
