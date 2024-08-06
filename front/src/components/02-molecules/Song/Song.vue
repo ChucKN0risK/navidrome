551b7baa1bd153475817ee2bdbd88fe3a7a3c2bd
Louis Chenais <lchenais@gmail.com>
2024-08-06 12:15:55 +0200

    Improve Song component typing + emit song loading event

--- front/src/components/02-molecules/Song/Song.vue
+++ front/src/components/02-molecules/Song/Song.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="m-song" @click.prevent="setCurrentTrack(song.id)">
+  <button class="m-song" @click.prevent="setPlayingSong">
     <AlbumCover
       v-if="albumCover"
       class="m-song__cover"
@@ -10,15 +10,15 @@
       <span v-if="showTrackNumber">{{ song.track }}</span>
       <span class="m-song__info__title">{{ song.title }}</span>
       <span v-if="showArtist" class="m-song__info__artist">{{ song.artist }}</span>
-      <span class="m-song__info__duration">{{ secondsToHHMMSS(song.duration) }}</span>
+      <span class="m-song__info__duration">{{ songDuration }}</span>
     </div>
   </button>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { secondsToHHMMSS } from '@/utils/timeConverter.utils';
 import { type Child } from 'subsonic-api';
-import { usePlayerStore } from '@/stores/player';
 import AlbumCover from '@/components/01-atoms/AlbumCover/AlbumCover.vue';
 
 const props = withDefaults(
@@ -26,7 +26,7 @@ const props = withDefaults(
     song: Child;
     showTrackNumber?: boolean;
     showArtist?: boolean;
-    albumCover?: string | undefined;
+    albumCover?: string;
   }>(),
   {
     showTrackNumber: false,
@@ -34,9 +34,16 @@ const props = withDefaults(
   }
 );
 
-const { setSongId } = usePlayerStore();
-const setCurrentTrack = (songId: string) => {
-  setSongId(songId);
+const songDuration = computed(() => {
+  return props.song.duration ? secondsToHHMMSS(props.song.duration) : false;
+})
+
+const emit = defineEmits<{
+  (emitName: 'set-playing-song', songId: string): void;
+}>();
+
+const setPlayingSong = () => {
+  emit('set-playing-song', props.song.id);
 };
 </script>
 
