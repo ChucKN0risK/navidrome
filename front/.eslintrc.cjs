abf48fa4917ec3aa2bdb07b7a083d79d9082255e
Louis Chenais <lchenais@gmail.com>
2024-08-06 12:10:45 +0200

    Disable rule on Vue component name

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -3,7 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:storybook/recommended'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
