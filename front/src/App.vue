bb8760ceec7ddf0503195f61b827311eebaa921f
Louis Chenais <lchenais@gmail.com>
2024-08-23 17:36:07 +0200

    Clean App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,14 +7,9 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { useDrawerStore } from '@/stores/drawer';
-import { storeToRefs } from 'pinia';
 import Drawer from '@/components/03-organisms/Drawer/Drawer.vue';
 import Player from '@/components/03-organisms/Player/Player.vue';
 import MainNav from '@/components/03-organisms/MainNav/MainNav.vue';
-
-const { getDrawerOpenState } = storeToRefs(useDrawerStore());
-const drawerIsOpened = getDrawerOpenState;
 </script>
 
 <style lang="scss">
